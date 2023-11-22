814922c423cd853ef1e4d9cc3e6e2003ce67a257
faris8891 <salmanulfaris.villoor@gmail.com>
2023-11-22 22:38:02 +0530

    feat: fixed error handler and auth

--- Controllers/ProductsController.js
+++ Controllers/ProductsController.js
@@ -3,6 +3,7 @@ const productsModal = require("../Models/products");
 const productController = {
   addProducts: async (req, res) => {
     const dealerId = req.body?.id;
+    console.log(dealerId);
     const {
       productName,
       price,
@@ -19,12 +20,12 @@ const productController = {
       productName: productName,
       price: price,
       brand: brand,
-      // category: category,
+      category: category,
       stock: stock,
       defaultImage: defaultImage,
       productImages: productImages,
       description: description,
-      // tag: tag,
+      tag: tag,
     });
 
         res.status(201).json({
