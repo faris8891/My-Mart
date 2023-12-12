4fd5216aeb36ad6273531892f0f8e2771d774bb9
faris8891 <salmanulfaris.villoor@gmail.com>
2023-12-12 22:31:25 +0530

    feat: dealer can update order status

--- Routers/orders.js
+++ Routers/orders.js
@@ -3,8 +3,19 @@ const { authentication } = require("../Middleware/authentication");
 const router = express.Router();
 const { tryCatch } = require("../Middleware/tryCatch");
 
-const {createCodOrders} = require("../Controllers/ordersControllers");
+const {
+  createCodOrders,
+  createOnlineOrders,
+  updateOrderStatus,
+} = require("../Controllers/ordersControllers");
 
 router.route("/cod").post(authentication.users, tryCatch(createCodOrders));
+router
+  .route("/online")
+  .post(authentication.users, tryCatch(createOnlineOrders));
+
+router
+  .route("/status/:orderId")
+  .patch(authentication.dealers, tryCatch(updateOrderStatus));
 
 module.exports = router;
